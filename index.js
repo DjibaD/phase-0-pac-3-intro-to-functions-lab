1bd0485cc1b9056b18f7cb54b255a1b4f33d6765
djibadiawara <djiba.diawara97@gmail.com>
2021-09-24 22:08:42 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+"Hello!".toUpperCase();
+function shout(string) {
+    return string.toUpperCase();
+  } 
+  "Hello!".toLowerCase(); 
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+ const string = "Hello!"; 
+  function logShout (string) {
+    console.log(string.toUpperCase());
+  }
+  function logWhisper (string) {
+    console.log(string.toLowerCase());
+  } 
+  function sayHiToGrandma(string){
+    if (string === string.toLowerCase()){
+      var lowerCase = string.toLowerCase();
+      return ('I can\'t hear you!');
+    }
+    else if (string === string.toUpperCase()){
+      var upperCase = 'YES INDEED!';
+      return ('YES INDEED!');
+      }
+      else{
+        var mixedCase = 'I love you, too.';
+        return(mixedCase);
+      }
+    }
